3e1cd75416c5ab4df81236d39ba4b555909f3a60
PakkPitak <frankypakkawat@gmail.com>
2024-08-08 23:57:10 +0700

    3.7

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,8 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 
 import StudentListView from '@/views/StudentListView.vue'
+import EventService from '@/services/EventService'
+import { useEventStore } from '@/stores/event'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +27,25 @@ const router = createRouter({
       name: 'event-layout-view',
       component: EventLayoutView,
       props: true,
+      beforeEnter: (to) => {
+        const id = parseInt(to.params.id as string)
+        const eventStore = useEventStore()
+        return EventService.getEvent(id)
+        .then((response) => {
+          // need to setup the data for the event
+          eventStore.setEvent(response.data)
+        })
+        .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            return {
+              name: '404-resource-view',
+              params: { resource: 'event' }
+            }
+          } else {
+            return { name: 'network-error-view' }
+          }
+        })
+      },
       children: [
         {
           path: '',
